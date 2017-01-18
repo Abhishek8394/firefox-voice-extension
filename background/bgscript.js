13865f1c6055ba1814503019364d95d5101c8407
patel-abhishek <asp15c@my.fsu.edu>
2017-01-18 11:24:41 -0500

    Basic background script skeleton

--- background/bgscript.js
+++ background/bgscript.js
@@ -7,6 +7,7 @@ function newTabHandler(tab){
 // Load Content scripts and other stuff to prepare for voice control
 function initializeTab(t){
 	//TODO load content scripts
+	// browser.tabs.update(t.id,{url:"https://www.google.com"});
 }
 
 // Perform initialization of all existing tabs
@@ -27,5 +28,27 @@ function initializeExistingTabs(){
 	tablist.then(tabQueryHandler,tabQueryErrorHandler);
 }
 
-browser.tabs.onCreated.addListener(newTabHandler);
-initializeExistingTabs();
+function connectBackend(){	
+	var wbsock = new WebSocket("ws://127.0.0.1:8080/"+"hi");
+	wbsock.onerror = function(err){
+		console.log("err");
+	};
+	wbsock.onmessage = function(event){
+		console.log(event.data);
+	}
+	wbsock.onopen = function(event){	
+		wbsock.send("hello");
+		console.log("writing socket");
+	};
+	console.log("socket defined");
+	logMessage("socket defined");
+
+}
+
+function globalInit(){	
+	browser.tabs.onCreated.addListener(newTabHandler);
+	initializeExistingTabs();
+	connectBackend();
+}
+
+globalInit();
---
+++ background/intentMappings.js
@@ -0,0 +1,6 @@
+// each entry => "intent delivered by voice engine": "scriptName:method"
+var intent2script = {
+	"create_tab":"tabManager:create_tab",
+	"close_tab":"tabManager:close_tab",
+	"search_intent":"search:search"
+};
