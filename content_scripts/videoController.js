3363d5823539295006cfe6858b4c512be5692b27
Abhishek Patel <abhidgreat8394@gmail.com>
2017-01-29 19:16:53 -0500

    Basic Video Controller

--- content_scripts/videoController.js
+++ content_scripts/videoController.js
@@ -15,6 +15,54 @@ function pause(msg){
 	}
 }
 
+function muteToggler(msg,toMute){
+	var videos = getVideoElement(msg);
+	if(videos.length==0){return;}
+	for(video of videos){
+		video.muted = toMute;
+	}	
+}
+
+function volumeSetter(msg,volumeLevel){
+	if(volumeLevel==undefined){
+		return;
+	}
+	volumeLevel = volumeLevel.value;
+	console.log(volumeLevel);
+	var vols = {"high":1,"low":0.1,"med":0.5};
+	var videos = getVideoElement(msg);
+	var newVolume = vols[volumeLevel];
+	if(videos.length==0){return;}
+	for(video of videos){
+		if(newVolume==undefined){
+			switch(volumeLevel){
+				case "inc":
+					video.volume = Math.min(1,video.volume+0.2);
+					break;
+				case "dec":
+					console.log(video.volume+" dec freg");
+					video.volume = Math.max(0,video.volume-0.2);
+					console.log(video.volume);
+					break;
+			}
+		}
+		else{
+			video.volume = newVolume;
+		}
+		
+	}		
+}
+
+function timeSeeker(msg,incOrDec){
+	var seekTimeDelta = msg.getSlot("seek");
+	seekTimeDelta = seekTimeDelta==undefined?5:parseInt(seekTimeDelta.value);
+	var videos = getVideoElement(msg);
+	if(videos.length==0){return;}
+	for(video of videos){
+		video.currentTime += incOrDec*seekTimeDelta;
+	}	
+}
+
 function getVideoElement(msg){
 	var videos = document.getElementsByTagName("video");
 	return videos;
@@ -37,6 +85,31 @@ function videoController(msg){
 			pause(msg);
 			addSessionAttribute(msg,"player_status","pausing");
 			sendMessageToVop(msg,"pausing","");
+			break;
+		case "mute":
+			muteToggler(msg,true);
+			addSessionAttribute(msg,"volume_status","muting");
+			sendMessageToVop(msg,"muting","");
+			break;
+		case "unmute":
+			muteToggler(msg,false);
+			addSessionAttribute(msg,"volume_status","unmuting");
+			sendMessageToVop(msg,"unmuting","");
+			break;
+		case "volume":
+			volumeSetter(msg,msg.getSlot("volumeLevel"));
+			addSessionAttribute(msg,"volume_status","changing");
+			sendMessageToVop(msg,"changing","");
+			break;
+		case "fwd":
+			timeSeeker(msg,1);
+			addSessionAttribute(msg,"seek_status","changing");
+			sendMessageToVop(msg,"fwding","");
+			break;
+		case "rewind":
+			timeSeeker(msg,-1);
+			addSessionAttribute(msg,"seek_status","changing");
+			sendMessageToVop(msg,"rewinding","");
 			break;
 	}
 	console.log("finally");
