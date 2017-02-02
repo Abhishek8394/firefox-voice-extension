c50bb3b68f6e65d499806a398d498561c112422a
Abhishek Patel <abhidgreat8394@gmail.com>
2017-02-01 21:28:15 -0500

    imporoved from filling+select options+refactoring
    Form filling done properly based on input type.
    When inputing a <select> element, options displayed as a banner,
    refactoring of code.

--- content_scripts/FormFiller.js
+++ content_scripts/FormFiller.js
@@ -1,78 +1,138 @@
 var formFillerSession = new Session();
+const FormConstants={
+	// sessions keys
+	ALL_FORMS_KEY:"forms_all",
+	CURR_FORM_KEY:"current_form",
+	FORM_ASKING_KEY:"asking_form_index",
+	ACTIVE_FORM_OFFSET_KEY:"selected_form_index",
+	INPUT_OFFSET_KEY:"input_element_index",
+	ACTIVE_FORM_INPUTS_KEY:"form_input_fields",
+	INPUT_ACTIVE_KEY:"current_input_field",
+	SELECT_OPTIONS_DISPLAYER:"select_options_displayer",
+	// user request keys
+	MSG_CHOSEN_FORM_INDEX:"chooseFormIndex",
+	MSG_CHOSEN_INPUT_INDEX:"chooseInputIndex",
+	MSG_USER_INPUT:"formInput"
+};
+
+// This function displays a list on screen.
+// TO remove existing select, just pas undefined, false
+function selectElementsToggleDisplay(selectElement,show=true){
+	if(selectElement==undefined){
+		if(formFillerSession.get(FormConstants.SELECT_OPTIONS_DISPLAYER)!=undefined){
+			// console.log("cached selct");
+			formFillerSession.get(FormConstants.SELECT_OPTIONS_DISPLAYER).style.display=show?"block":"none";
+		}
+		return;
+	}
+	// console.log("new select");
+	var selectOptions = getPromptDisplayBanner();
+	var opts = [];
+	var ctr=0;
+	for(option of selectElement.options){
+		opts.push("<span>"+ctr+": "+option.innerHTML+"</span><br>");
+		ctr+=1;
+	}
+	selectOptions.innerHTML = opts.join("");
+	document.getElementsByTagName("body")[0].appendChild(selectOptions);
+	formFillerSession.add(FormConstants.SELECT_OPTIONS_DISPLAYER,selectOptions);
+}
 
 // Get all forms in a page
 function captureAllForms(){
 	var forms = $('form:visible');
 	if(forms!=undefined && forms.length>0){
-		formFillerSession.add("all_forms",forms);
+		formFillerSession.add(FormConstants.ALL_FORMS_KEY,forms);
 	}
 	console.log(forms);
 	if(forms.length==1){
-		formFillerSession.add('current_form',forms[0]);
+		formFillerSession.add(FormConstants.CURR_FORM_KEY,forms[0]);
 	}
-	formFillerSession.add("asking_form_index",0);
+	formFillerSession.add(FormConstants.FORM_ASKING_KEY,0);
 }
 
 // Interact with user to decide on a form to fill up.
 // TODO cancel request.
 function pickForm(msg){
-	var forms = formFillerSession.get("all_forms");
-	var currIndex = formFillerSession.get("asking_form_index");
-	var userChosenIndex = msg.getSlot("chooseFormIndex");
-	var yesHandler = function(msg,userReply){
+	var forms = formFillerSession.get(FormConstants.ALL_FORMS_KEY);
+	var currIndex = formFillerSession.get(FormConstants.FORM_ASKING_KEY);
+	var userChosenIndex = msg.getSlot(FormConstants.MSG_CHOSEN_FORM_INDEX);
+	var formYesHandler = function(msg,userReply){
 		console.log("said yes");
-		formFillerSession.add("current_form",forms[currIndex]);
-		formInputHandler(msg);
+		formFillerSession.add(FormConstants.CURR_FORM_KEY,forms[currIndex]);
+		// formInputHandler(msg);
 	};
-	var noHandler = function(msg,userReply){
+	var formNoHandler = function(msg,userReply,direction=1){
 		unHighlightElement(forms[currIndex]);
-		currIndex = (currIndex+1)%forms.length;
+		currIndex = (currIndex+direction)%forms.length;
 		addSessionAttribute(msg,"asking_form",currIndex);
-		formFillerSession.add('asking_form_index',currIndex);
+		formFillerSession.add(FormConstants.FORM_ASKING_KEY,currIndex);
 		highlightElement(forms[currIndex]);
+		formFillerSession.add(FormConstants.CURR_FORM_KEY,undefined);
+		formFillerSession.add(FormConstants.INPUT_ACTIVE_KEY,undefined);
+		formFillerSession.add(FormConstants.INPUT_OFFSET_KEY,undefined);
 		sendMessageToVop(msg,"Perform input on form number "+(currIndex+1)+"?","Answer in a yes or no");
+		selectElementsToggleDisplay(undefined,false);
+	};
+	var formNextHandler = function(msg,userReply){
+		formNoHandler(msg,userReply,1);
+	};
+	var formPrevHandler=function(msg,userReply){
+		formNoHandler(msg,userReply,-1);
+	};
+	var formCancelHandler = function(msg,userReply){
+		formNoHandler(msg,userReply,1);
 	};
 	var formReplyHandlers = [
-		{name:"yes",handler:yesHandler},
-		{name:"no",handler:noHandler}
+		{name:"yes",handler:formYesHandler},
+		{name:"no",handler:formNoHandler},
+		{name:"next",handler:formNextHandler},
+		{name:"previous",handler:formPrevHandler},
+		{name:"cancel",handler:formCancelHandler}
 	];
-	ElementPicker(msg,forms,"asking_form_index",
-		"chooseFormIndex",formFillerSession,formReplyHandlers,true,"red");
+	ElementPicker(msg,forms,FormConstants.FORM_ASKING_KEY,
+		FormConstants.MSG_CHOSEN_FORM_INDEX,formFillerSession,formReplyHandlers,true,"red");
 }
 
 // get all input elements of a form
 function getInputElements(form){
 	// If the form we are trying to fill is still the one the input elements think they belong to, return that list. 
 	// return cached copy of available input fields.
-	if(formFillerSession.get("formInputFields")!=undefined && formFillerSession.get("fillingFormId")!=undefined && 
-		formFillerSession.get("fillingFormId")==formFillerSession.get("asking_form_index")){
+	if(formFillerSession.get(FormConstants.ACTIVE_FORM_INPUTS_KEY)!=undefined && formFillerSession.get(FormConstants.ACTIVE_FORM_OFFSET_KEY)!=undefined && 
+		formFillerSession.get(FormConstants.ACTIVE_FORM_OFFSET_KEY)==formFillerSession.get(FormConstants.FORM_ASKING_KEY)){
 		console.log("served from cache");
-		return formFillerSession.get("formInputFields");
+		return formFillerSession.get(FormConstants.ACTIVE_FORM_INPUTS_KEY);
 	}
 	// Collect all fillable form elements, cache the copy along with the form index.
 	var inputs = form.getElementsByTagName("input");
 	var selects = form.getElementsByTagName("select");
 	var buttons = form.getElementsByTagName("button");
-	var formsInput = {
-		inputs:inputs,
-		selects:selects,
-		buttons:buttons
-	};
+	var formsInputByType = [
+		inputs,
+		selects,
+		buttons
+	];
+	var formsInput = [];
+	for(formInputList of formsInputByType){
+		for( formInput of formInputList){
+			formsInput.push(formInput);
+		}
+	}
 	// Hold the collected input Elements
-	formFillerSession.add("formInputFields",formsInput);
+	formFillerSession.add(FormConstants.ACTIVE_FORM_INPUTS_KEY,formsInput);
 	// A pointer to form which current element list is populated. 
 	// Required because inputElement list is cached and needs to be refreshed when 
 	// target form changes.
-	formFillerSession.add("fillingFormId",formFillerSession.get("asking_form_index"));
+	formFillerSession.add(FormConstants.ACTIVE_FORM_OFFSET_KEY,formFillerSession.get(FormConstants.FORM_ASKING_KEY));
 	// Reset the values
 	formFillerSession.add("input_group",undefined);
-	formFillerSession.add("input_element_index",undefined);
+	formFillerSession.add(FormConstants.INPUT_OFFSET_KEY,undefined);
 	return formsInput;
 }
 
 // See if user allowed to enter input on this element. For now, filter out hidden elements.
 function isLegitInput(element){
-	if(element.attributes.type.value!="hidden"){
+	if(element.attributes.type==undefined || element.attributes.type.value!="hidden"){
 		return true;
 	}
 	return false;
@@ -80,28 +140,38 @@ function isLegitInput(element){
 
 // Enter value in a form input. 
 function performFormInput(msg){
-	var form = formFillerSession.get('current_form');
-	var inputGroup = formFillerSession.get("input_group");
-	var elementOffset = formFillerSession.get("input_element_index");
-	var userInput = msg.getSlot("formInput")==undefined?undefined:msg.getSlot("formInput").value;
-	var inputElement = formFillerSession.get("current_input_field");
-	console.log(inputGroup);
+	var form = formFillerSession.get(FormConstants.CURR_FORM_KEY);
+	// var inputGroup = formFillerSession.get("input_group");
+	var elementOffset = formFillerSession.get(FormConstants.INPUT_OFFSET_KEY);
+	var userInput = msg.getSlot(FormConstants.MSG_USER_INPUT)==undefined?undefined:msg.getSlot(FormConstants.MSG_USER_INPUT).value;
+	var inputElement = formFillerSession.get(FormConstants.INPUT_ACTIVE_KEY);
+	// console.log(inputGroup);
 	console.log(elementOffset);
 	console.log(userInput);
 	console.log(inputElement);
 	inputElement.focus();
-	switch(inputGroup){
-		case "inputs":
-			inputElement.click();
+	switch(inputElement.tagName){
+		case "INPUT":
 			userInput = userInput==undefined?inputElement.value:userInput;;
-			inputElement.value += userInput;
+			if(inputElement.type=="radio" || inputElement.type=="checkbox"){
+				if(userInput!=undefined && isSayingYes(userInput)==true){
+					inputElement.checked=true;
+				}
+				else if(userInput!=undefined && isSayingYes(userInput)==false){
+					inputElement.checked = false;
+				}
+			}
+			else{				
+				inputElement.click();
+				inputElement.value += userInput;
+			}
 			break;			
-		case "selects":
+		case "SELECT":
 			inputElement.click();
 			userInput = userInput==undefined?"0":userInput;
 			inputElement.options[parseInt(userInput)].selected=true;
 			break;
-		case "buttons":
+		case "BUTTON":
 			userInput = userInput==undefined || userInput!="true"?false:true;
 			if(userInput)
 			{
@@ -112,46 +182,93 @@ function performFormInput(msg){
 	return true;
 }
 
+// Check if user issued a valid command for form navigation.
+function validFormNavigationRequest(msg){
+	var validRequests = ["yes","no","next","previous","cancel"];
+	var userCommand = msg.getSlot(FormConstants.MSG_CHOSEN_FORM_INDEX);
+	if(userCommand!=null){
+		userCommand=userCommand.value.trim().toLowerCase();
+		for(req of validRequests){
+			if(req==userCommand){
+				console.log(userCommand);console.log(validRequests);
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+// Prompt text for elements
+function getPromptText(inputElement){
+	var promptText="";
+	switch(inputElement.tagName){
+		case "INPUT":
+			if(inputElement.type=="radio"){
+				promptText = "select this option?";
+			}
+			else if(inputElement.type=="checkbox"){
+				promptText = "select this option?";
+			}
+			else{
+				promptText = "select this element?";
+			}
+			break;			
+		case "SELECT":
+			promptText = "Pick an option";
+			break;
+		case "BUTTON":
+			promptText = "Press this button?";
+			break;
+	}
+	return promptText;
+}
+
+
 function formInputHandler(msg){
-	console.log("hello");
+	// selectElementsToggleDisplay(document.getElementsByTagName("select")[0],true);
 	msg = new VopMessage(msg,undefined);
+	console.log(msg);
 	// check if a form decided for input
-	if(formFillerSession.get('current_form')==undefined){
+	if(formFillerSession.get(FormConstants.CURR_FORM_KEY)==undefined || validFormNavigationRequest(msg)){
+		console.log("on form "+formFillerSession.get(FormConstants.CURR_FORM_KEY));
 		pickForm(msg);
-		return;
+		// return;
 	}
 	console.log("all set to operate!");
-	var form = formFillerSession.get('current_form');
+	var form = formFillerSession.get(FormConstants.CURR_FORM_KEY);
+	if(form==undefined){
+		return;
+	}
 	var inpElements = getInputElements(form);
 	var askForInput = true;
 	console.log(inpElements);
 	// loop through elements of chosen form.
 	// For separate treatment of inputs, selects and buttons
-	var inputGroup = formFillerSession.get("input_group");
-	var elementOffset = formFillerSession.get("input_element_index");
+	// var inputGroup = formFillerSession.get("input_group");
+	var elementOffset = formFillerSession.get(FormConstants.INPUT_OFFSET_KEY);
 	// If msg contains input for a input field.
-	if(msg.getSlot("formInput")!=undefined && formFillerSession.get("current_input_field")!=undefined){
+	if(msg.getSlot(FormConstants.MSG_USER_INPUT)!=undefined && formFillerSession.get(FormConstants.INPUT_ACTIVE_KEY)!=undefined){
 		askForInput = performFormInput(msg);
 	}
-	if(inputGroup==undefined){
-		// get first non empty input group
-		for(group of Object.keys(inpElements)){
-			if(inpElements[group]!=undefined && inpElements[group].length>0){
-				inputGroup = group;
-				formFillerSession.add("input_group",inputGroup);
-				break;
-			}
-		}
-	}
+	// if(inputGroup==undefined){
+	// 	// get first non empty input group
+	// 	for(group of Object.keys(inpElements)){
+	// 		if(inpElements[group]!=undefined && inpElements[group].length>0){
+	// 			inputGroup = group;
+	// 			formFillerSession.add("input_group",inputGroup);
+	// 			break;
+	// 		}
+	// 	}
+	// }
 	
 	if(elementOffset==undefined){
-		console.log(inpElements[inputGroup]);
+		console.log(inpElements);
 		var tmpCtr = 0;
-		for(element of inpElements[inputGroup]){
+		for(element of inpElements){
 			if(isLegitInput(element)){				
 				elementOffset=tmpCtr;
-				formFillerSession.add("input_element_index",elementOffset);	
-				highlightElement(inpElements[inputGroup][elementOffset],"green");	
+				formFillerSession.add(FormConstants.INPUT_OFFSET_KEY,elementOffset);	
+				highlightElement(inpElements[elementOffset],"green");	
 				break;
 			}
 			tmpCtr+=1;
@@ -159,23 +276,29 @@ function formInputHandler(msg){
 	}
 	// When asking to confirm a input element and user said yes
 	var inputYesHandler = function(msg,userReply){
-		var chosenInputField = inpElements[inputGroup][elementOffset];
+		var chosenInputField = inpElements[elementOffset];
 		console.log(chosenInputField);
-		formFillerSession.add("current_input_field",chosenInputField);		
+		formFillerSession.add(FormConstants.INPUT_ACTIVE_KEY,chosenInputField);		
+		if(chosenInputField.tagName=="SELECT"){
+			selectElementsToggleDisplay(chosenInputField);
+		}
 	};
 	// When asking to confirm a input Element and user says no
 	var inputNoHandler = function(msg,userReply,movementDirection = 1){
-		unHighlightElement(inpElements[inputGroup][elementOffset]);
+		unHighlightElement(inpElements[elementOffset]);
 		var currOffset = elementOffset;
-		elementOffset = (elementOffset+movementDirection)%inpElements[inputGroup].length;
-		while(!isLegitInput(inpElements[inputGroup][elementOffset])){
-			elementOffset = (elementOffset+movementDirection)%inpElements[inputGroup].length;
+		elementOffset = (elementOffset+movementDirection)%inpElements.length;
+		while(!isLegitInput(inpElements[elementOffset])){
+			elementOffset = (elementOffset+movementDirection)%inpElements.length;
 			if(elementOffset==currOffset){break;}
 		}
-		addSessionAttribute(msg,"input_element_index",elementOffset);
-		formFillerSession.add('input_element_index',elementOffset);
-		highlightElement(inpElements[inputGroup][elementOffset],"green");
-		sendMessageToVop(msg,"Perform input on element number "+(elementOffset+1)+"?","Answer in a yes or no");
+		addSessionAttribute(msg,FormConstants.INPUT_OFFSET_KEY,elementOffset);
+		formFillerSession.add(FormConstants.INPUT_OFFSET_KEY,elementOffset);
+		highlightElement(inpElements[elementOffset],"green");
+		formFillerSession.add(FormConstants.INPUT_ACTIVE_KEY,undefined);
+		var promptText = getPromptText(inpElements[elementOffset]);	
+		sendMessageToVop(msg,promptText,promptText);
+		selectElementsToggleDisplay(undefined,false);
 	};
 	var inputPrevHandler = function(msg,userReply){
 		inputNoHandler(msg,userReply,-1);
@@ -189,11 +312,12 @@ function formInputHandler(msg){
 		{ name:"next", handler:inputNextHandler },
 		{ name:"previous", handler:inputPrevHandler }
 	];
-	if(askForInput){		
-		ElementPicker(msg,inpElements[inputGroup],"input_element_index","chooseInputIndex",formFillerSession,inputReplyHandlers,true,"green");
+	if(askForInput){	
+		var promptText = getPromptText(inpElements[elementOffset]);
+		ElementPicker(msg,inpElements,FormConstants.INPUT_OFFSET_KEY,FormConstants.MSG_CHOSEN_INPUT_INDEX,formFillerSession,inputReplyHandlers,true,"green",promptText);
 	}
 	console.log("input ready on ");
-	console.log(inpElements[inputGroup][elementOffset]);
+	console.log(inpElements[elementOffset]);
 	// Ask for input on a form. If out of input fields ask for next form, else wait for submit command.
 }
 
--- content_scripts/contentUtility.js
+++ content_scripts/contentUtility.js
@@ -24,10 +24,14 @@ function unHighlightElement(element){
 
 // Interact with user to decide on a option to choose. 
 // TODO cancel request, access by index number
-function ElementPicker(msg,elementList,currIndexKey,userReplySlotKey,sessionObject,expectedReplyHandlers,shouldHighlight=true,highlightColor="red"){
+function ElementPicker(msg,elementList,currIndexKey,userReplySlotKey,sessionObject,expectedReplyHandlers,shouldHighlight=true,highlightColor="red",promptText,reprompt){
 	// var forms = formFillerSession.get("all_forms");
 	var currIndex = sessionObject.get(currIndexKey);
 	var userReply = msg.getSlot(userReplySlotKey);
+	if(promptText==undefined){
+		promptText = "Perform input on element "+(currIndex+1)+"?";
+	}
+	reprompt=reprompt==undefined?promptText:reprompt;
 	// console.log(userChosenIndex);
 	if(userReply==undefined){
 		console.log("said nothing");
@@ -36,7 +40,7 @@ function ElementPicker(msg,elementList,currIndexKey,userReplySlotKey,sessionObje
 		if(shouldHighlight){			
 			highlightElement(elementList[currIndex],highlightColor);
 		}
-		sendMessageToVop(msg,"Perform input on element "+(currIndex+1)+"?","Answer in a yes or no");
+		sendMessageToVop(msg,promptText,reprompt);
 		return;
 	}
 	for(possibleReply of expectedReplyHandlers){
@@ -44,4 +48,38 @@ function ElementPicker(msg,elementList,currIndexKey,userReplySlotKey,sessionObje
 			possibleReply.handler(msg,userReply.value);
 		}
 	}
+}
+
+// If user is saying yes, no or something else.
+function isSayingYes(responseString){
+	responseString = responseString.trim().toLowerCase();
+	var positiveWords = ["yes","ok","true"];
+	var negativeWords = ["no","nope"];
+	for(i of positiveWords){
+		if(i==responseString){
+			return true;
+		}
+	}
+	for(i of negativeWords){
+		if(i==responseString){
+			return false;
+		}
+	}
+	return undefined;
+}
+
+function getPromptDisplayBanner(){
+	var banner = document.createElement("div");
+	banner.id="foxy-prompt";
+	banner.style.height =  "50%";
+	banner.style.position = "fixed";
+	banner.style.top="25%";
+	banner.style.left="25%";
+	banner.style.overflow="scroll";
+	banner.zIndex=1000;
+	banner.style.background="#FFF";
+	banner.style.borderRadius="5px";
+	banner.style.boxShadow="6px 6px 10px #333";
+	banner.style.padding="2%";
+	return banner;
 }
